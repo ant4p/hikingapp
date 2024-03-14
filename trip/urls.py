6b022048be6ae06397b0338f3979771064c580a0
= <=>
2024-03-14 19:02:01 +0300

    fix urls

--- trip/urls.py
+++ trip/urls.py
@@ -4,8 +4,10 @@ from trip.views import TripList, ShowTrip, AddTrip
 
 urlpatterns = [
     path('', TripList.as_view(), name='home'),
-    path('trip/<slug:slug>/', ShowTrip.as_view(), name='trip'),
     path('add/', AddTrip.as_view(), name='add'),
+
+    path('trip/<slug:slug>/', ShowTrip.as_view(), name='trip'),
+
 
 ]
 
