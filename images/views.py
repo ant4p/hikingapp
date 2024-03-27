09c5db70b856b6fab32569cb0401cd4a352b9d78
ant4p <enter4p@gmail.com>
2024-03-27 20:36:22 +0300

    small bugfixes

--- images/views.py
+++ images/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+
 from django.urls import reverse_lazy, reverse
-from django.views.generic import FormView
+from django.views.generic import FormView, DetailView
 
 from images.forms import AddImagesForm
 
@@ -11,11 +12,13 @@ from trip.models import Trip
 class AddImages(FormView):
     form_class = AddImagesForm
     template_name = 'images/upload_image.html'
-    # queryset = Trip.objects.filter(slug='slug')
+
     success_url = reverse_lazy('home')
     # context_object_name = 'images'
 
     def post(self, request, *args, **kwargs):
+        # trip = Trip.objects.get(slug='slug')
+
         form_class = self.get_form_class()
         form = self.get_form(form_class)
         if form.is_valid():
@@ -24,11 +27,13 @@ class AddImages(FormView):
             return self.form_invalid(form)
 
     def form_valid(self, form):
+
         files = form.cleaned_data["file_field"]
-
+        print(files)
         for f in files:
-            Image(image=f).save()
 
+            Image.objects.create(image=f)
+            # Trip.f.images.add(files)
 
         return super().form_valid(form)
 
