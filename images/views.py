6bb81af468e997207b57af230168991f7a3bdeff
ant4p <enter4p@gmail.com>
2024-05-15 18:32:56 +0300

    small pagination & gallery update

--- images/views.py
+++ images/views.py
@@ -14,7 +14,7 @@ class ShowGallery(ListView):
     def get_queryset(self):
         images = Image.objects.all()
         try:
-            random_images = random.choices(images, k=12)
+            random_images = random.choices(images, k=8)
             return random_images
         except IndexError:
             pass
